fe31147955bdb9500c202174bd82a5ec737cbb85
Karel De Ridder <Karel.DeRidder@rodekruis.be>
2020-09-15 13:41:22 +0200

    move images to sep folder

--- main.py
+++ main.py
@@ -10,24 +10,24 @@ from math_utilities import sin
 from math_utilities import cos
 from math_utilities import radians
 
-player_image = pyglet.image.load("red_circle_p_transparant.png")
+player_image = pyglet.image.load("images\\red_circle_p_transparant.png")
 player_image.anchor_x = int(player_image.width // 2)
 player_image.anchor_y = int(player_image.height // 2)
 
-shot_image = pyglet.image.load("shot_r_with_start_transparant.png")
+shot_image = pyglet.image.load("images\\shot_r_with_start_transparant.png")
 
-enemy_angry_image = pyglet.image.load("boss_angry_face_transparant.png")
+enemy_angry_image = pyglet.image.load("images\\boss_angry_face_transparant.png")
 enemy_angry_image.anchor_x = int(enemy_angry_image.width // 2)
 enemy_angry_image.anchor_y = int(enemy_angry_image.height // 2)
 
-enemy_shocked_image = pyglet.image.load("boss_shocked_face_transparant.png")
+enemy_shocked_image = pyglet.image.load("images\\boss_shocked_face_transparant.png")
 enemy_shocked_image.anchor_x = int(enemy_shocked_image.width // 2)
 enemy_shocked_image.anchor_y = int(enemy_shocked_image.height // 2)
 
-crosshair_image = pyglet.image.load("black_crosshair_transparant.png")
+crosshair_image = pyglet.image.load("images\\black_crosshair_transparant.png")
 cursor = pyglet.window.ImageMouseCursor(crosshair_image, 5, 5)
 
-buff_image = pyglet.image.load("basic_buff.png")
+buff_image = pyglet.image.load("images\\basic_buff.png")
 buff_image.anchor_x = int(buff_image.width // 2)
 buff_image.anchor_y = int(buff_image.height // 2)
 
